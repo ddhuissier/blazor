5da4c3f10c37ac5aa4cc18bd246a19b6e22ac748
ddhuissier <ddhuissier@gmail.com>
2021-08-29 15:15:10 +0200

    Authentification + Authorization +MatBlazor + Consume RCL (Razor Class Library).

---
+++ BlazorServer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using BlazorServer.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlazorServer.Areas.Identity.IdentityHostingStartup))]
+namespace BlazorServer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BlazorServerContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BlazorServerIdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BlazorServerContext>();
+            });
+        }
+    }
+}
