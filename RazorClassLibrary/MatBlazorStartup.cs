da51b302e3c4f86b5a5f91c6deb5398e6a043766
ddhuissier <ddhuissier@gmail.com>
2021-09-20 08:57:27 +0200

    Stable version

---
+++ RazorClassLibrary/MatBlazorStartup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using MatBlazor;
+
+namespace RazorClassLibrary
+{
+    public class MatBlazorStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            //builder.ConfigureServices((context, services) => {
+            //    services.AddMatToaster(config =>
+            //    {
+            //        config.Position = MatToastPosition.BottomRight;
+            //        config.PreventDuplicates = true;
+            //        config.NewestOnTop = true;
+            //        config.ShowCloseButton = true;
+            //        config.MaximumOpacity = 95;
+            //        config.VisibleStateDuration = 3000;
+            //    });
+            //});
+        }
+    }
+}
